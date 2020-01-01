69ea5c9f968c5383a7d24674cbda88b6a5a418a1
Cuthbert20 <knowltonspencer@gmail.com>
2020-01-01 15:23:55 -0700

    first commit

---
+++ data/mockMembers.js
@@ -0,0 +1,54 @@
+const uuid = require("uuid");
+
+const members = [
+  {
+    id: uuid.v4(),
+    firstName: "Jesse",
+    lastName: "Cowgirl",
+    email: "jesse@me.com",
+    phone: "801-801-2356",
+    dogName: "Woofy",
+    vaccinations: true,
+    dogSize: "S"
+  },
+  {
+    id: uuid.v4(),
+    firstName: "Woody",
+    lastName: "Cowboy",
+    email: "woody@me.com",
+    phone: "435-435-6262",
+    dogName: "Bark",
+    vaccinations: true,
+    dogSize: "L"
+  },
+  {
+    id: uuid.v4(),
+    firstName: "Buzz",
+    lastName: "Lightyear",
+    email: "buzz@me.com",
+    phone: "435-623-1777",
+    dogName: "Alien",
+    vaccinations: true,
+    dogSize: "XL"
+  },
+  {
+    id: uuid.v4(),
+    firstName: "Forky",
+    lastName: "HaHa",
+    email: "forky@me.com",
+    phone: "801-111-1111",
+    dogName: "Spoon",
+    vaccinations: false,
+    dogSize: "L"
+  },
+  {
+    id: uuid.v4(),
+    firstName: "What",
+    lastName: "Noooooo",
+    email: "what.nooo@me.com",
+    phone: "111-222-3548",
+    dogName: "Coffee",
+    vaccinations: true,
+    dogSize: "M"
+  }
+];
