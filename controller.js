22c9fc722dbaa3212377fe5b3779e2d4f282d898
Cuthbert20 <knowltonspencer@gmail.com>
2020-01-01 17:11:24 -0700

    created routes to fetch member by id and create member

--- controller.js
+++ controller.js
@@ -1,7 +1,45 @@
 const members = require("./data/mockMembers");
+const uuid = require("uuid");
 
 module.exports = {
   getMembers: (req, res) => {
+    res.json(members);
+  },
+  getMember: (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+      res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+      res
+        .status(400)
+        .json({ msg: `No member with the id of ${req.params.id}` });
+    }
+  },
+  createMember: (req, res) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      dogName,
+      vaccinations,
+      dogSize
+    } = req.body;
+    const newMember = {
+      id: uuid.v4(),
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      dogName: dogName,
+      vaccinations: vaccinations,
+      dogSize: dogSize
+    };
+    console.log(newMember);
+    if (!newMember.firstName) {
+      return res.status(400).json({ msg: "Please include a First Name" });
+    }
+    members.push(newMember);
     res.json(members);
   }
 };
--- routes/members.js
+++ routes/members.js
@@ -5,5 +5,7 @@ const ctrl = require("../controller");
 //no need to use /api/members because of middleware on index.js
 
 router.get("/", ctrl.getMembers);
+router.get("/:id", ctrl.getMember);
+router.post("/", ctrl.createMember);
 
 module.exports = router;
